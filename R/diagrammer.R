69caa853412783ae757f0e0d18be65b5b620d2a7
Tom Palmer <remlapmot@hotmail.com>
2022-03-16 13:06:52 +0000

    Add multiple_iv_dag fn

--- R/diagrammer.R
+++ R/diagrammer.R
@@ -22,3 +22,32 @@ single_iv_dag <- function() {
       }
       ", height = 200)
 }
+
+#' @export
+multiple_iv_dag <- function() {
+  DiagrammeR::grViz("
+      digraph mrdag {
+
+      graph [rankdir=TB, layout=neato]
+
+      node [shape=ellipse]
+      U [label='Confounders', pos='3,1!']
+
+      node [shape=plaintext, height=0.3, width=0.3]
+      G1 [label='<I>G@_{1}</I>', pos='0,0.75!']
+      G2 [label='<I>G@_{2}</I>', pos='0,0!']
+      G3 [label='<I>G@_{3}</I>', pos='0,-0.75!']
+      X [label='Phenotype', pos='2,0!']
+      Y [label='Outcome', pos='4,0!']
+
+      { rank = same; G2 X Y }
+
+      G1 -> X 
+      G2 -> X 
+      G3 -> X 
+      U -> X
+      U -> Y
+      X -> Y 
+      }
+      ", height = 200)  
+}
