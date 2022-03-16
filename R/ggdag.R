a31ca91fa884c328c1dd826aa7765feb9a8cdd61
Tom Palmer <remlapmot@hotmail.com>
2022-03-16 13:49:50 +0000

    Add ggdag_multiple_iv_dag

--- R/ggdag.R
+++ R/ggdag.R
@@ -14,3 +14,34 @@ ggdag_single_iv_dag <- function() {
   ggmrdag <- ggdag::tidy_dagitty(mrdag)
   ggdag::ggdag(ggmrdag) + ggdag::theme_dag()
 }
+
+#' @importFrom ggdag dagify ggdag theme_dag
+#' @export
+ggdag_multiple_genotype_dag <- function() {
+  coords <- list(x = c(
+    G1 = 0,
+    G2 = 0,
+    G3 = 0,
+    X = 1,
+    Y = 2,
+    U = 1.5
+  ),
+  y = c(
+    G1 = 0.5,
+    G2 = 0,
+    G3 = -0.5,
+    X = 0,
+    Y = 0,
+    U = 1
+  ))
+  
+  mrdag <- ggdag::dagify(
+    X ~ G1 + G2 + G3 + U,
+    Y ~ X + U,
+    exposure = "X",
+    outcome = "Y",
+    coords = coords
+  )
+  
+  ggdag::ggdag(mrdag) + ggdag::theme_dag()
+}
