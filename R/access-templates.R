0f31b787efa3391ee55510a308b098f8081dad4a
Tom Palmer <remlapmot@hotmail.com>
2022-03-18 11:56:01 +0000

    Delete commented lines

--- R/access-templates.R
+++ R/access-templates.R
@@ -1,17 +1,9 @@
 #' @export
 tikz_template <- function() {
   system.file("templates", "tikz-template.tex", package = "mrdags")
-  # file.copy(from = system.file("templates", "tikz-template.tex", package = "mrdags"),
-  #           to = "tikz-template.tex",
-  #           overwrite = TRUE)
-  # "tikz-template.tex"
 }
 
 #' @export
 tikz_swig_template <- function() {
   system.file("templates", "tikz-template-swig-tr.tex", package = "mrdags")
-  # file.copy(from = system.file("templates", "tikz-template-swig-tr.tex", package = "mrdags"),
-  #           to = "tikz-template-swig-tr.tex",
-  #           overwrite = TRUE)
-  # "tikz-template-swig-tr.tex"
 }
