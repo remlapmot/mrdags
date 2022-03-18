c2e09e6c6a53658e4bc334228bdb59061edd4b33
Tom Palmer <remlapmot@hotmail.com>
2022-03-18 11:44:47 +0000

    Add tikz_swig_template fn

--- R/access-templates.R
+++ R/access-templates.R
@@ -7,3 +7,11 @@ tikz_template <- function() {
   # "tikz-template.tex"
 }
 
+#' @export
+tikz_swig_template <- function() {
+  system.file("templates", "tikz-template-swig-tr.tex", package = "mrdags")
+  # file.copy(from = system.file("templates", "tikz-template-swig-tr.tex", package = "mrdags"),
+  #           to = "tikz-template-swig-tr.tex",
+  #           overwrite = TRUE)
+  # "tikz-template-swig-tr.tex"
+}
