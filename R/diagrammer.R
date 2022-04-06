678a94a37e8591918a7b1ce2a07a143aea22f981
Tom Palmer <remlapmot@hotmail.com>
2022-04-06 11:40:13 +0100

    Add noborders option

--- R/diagrammer.R
+++ R/diagrammer.R
@@ -50,7 +50,8 @@ single_iv_dag <- function(noborders = TRUE) {
 
 #' @importFrom DiagrammeR grViz
 #' @export
-multiple_iv_dag <- function() {
+multiple_iv_dag <- function(noborders = TRUE) {
+  if (noborders) {
   DiagrammeR::grViz("
       digraph mrdag {
 
@@ -75,5 +76,32 @@ multiple_iv_dag <- function() {
       U -> Y
       X -> Y 
       }
-      ", height = 200)  
+      ", height = 200)
+  } else {
+  DiagrammeR::grViz("
+      digraph mrdag {
+
+      graph [rankdir=TB, layout=neato]
+
+      node [shape=ellipse]
+      U [label='Confounders', pos='3,1!']
+
+      node [shape=box, height=0.3, width=0.3]
+      G1 [label='<I>G@_{1}</I>', pos='0,0.75!']
+      G2 [label='<I>G@_{2}</I>', pos='0,0!']
+      G3 [label='<I>G@_{3}</I>', pos='0,-0.75!']
+      X [label='Phenotype', pos='2,0!']
+      Y [label='Outcome', pos='4,0!']
+
+      { rank = same; G2 X Y }
+
+      G1 -> X
+      G2 -> X
+      G3 -> X
+      U -> X
+      U -> Y
+      X -> Y
+      }
+      ", height = 200)
+  }
 }
