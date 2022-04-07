99b50864e8029eef4786b89e948e29b504cc2727
Tom Palmer <remlapmot@hotmail.com>
2022-04-07 15:09:50 +0100

    Formatting

--- R/ggdag.R
+++ R/ggdag.R
@@ -18,22 +18,8 @@ ggdag_single_iv_dag <- function() {
 #' @importFrom ggdag dagify ggdag theme_dag
 #' @export
 ggdag_multiple_iv_dag <- function() {
-  coords <- list(x = c(
-    G1 = 0,
-    G2 = 0,
-    G3 = 0,
-    X = 1,
-    Y = 2,
-    U = 1.5
-  ),
-  y = c(
-    G1 = 0.5,
-    G2 = 0,
-    G3 = -0.5,
-    X = 0,
-    Y = 0,
-    U = 1
-  ))
+  coords <- list(x = c(G1 = 0, G2 = 0, G3 = 0, X = 1, Y = 2, U = 1.5),
+                 y = c(G1 = 0.5, G2 = 0, G3 = -0.5, X = 0, Y = 0, U = 1))
   
   mrdag <- ggdag::dagify(
     X ~ G1 + G2 + G3 + U,
