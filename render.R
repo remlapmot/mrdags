ca6b5e8568643694d47dcea3815ef5c63a0befab
Tom Palmer <remlapmot@hotmail.com>
2024-09-17 10:16:41 +0100

    Update render.R

--- render.R
+++ render.R
@@ -1,5 +1,7 @@
 if (packageVersion('DiagrammeR') < '1.0.11') install.packages('DiagrammeR')
 
+rmarkdown::render('improved-latex.Rmd')
+
 # quarto::quarto_render()
 
 quarto::quarto_render(cache_refresh = TRUE)
