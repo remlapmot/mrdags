0a9d70ce459e452cae37c4b304d3cc4ae862fc4c
Tom Palmer <remlapmot@hotmail.com>
2024-09-17 10:51:53 +0100

    Update render.R

--- render.R
+++ render.R
@@ -1,5 +1,7 @@
 if (packageVersion('DiagrammeR') < '1.0.11') install.packages('DiagrammeR')
 
+if (!webshot::is_phantomjs_installed()) webshot::install_phantomjs()
+
 rmarkdown::render('improved-latex.Rmd')
 
 # quarto::quarto_render()
